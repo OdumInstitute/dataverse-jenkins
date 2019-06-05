af20eea1a9287c9277a5cf4c9c266f959c819a0c
Philip Durbin <philip_durbin@harvard.edu>
2019-06-05 18:50:48 -0400

    simplify and fix paths #1

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-PLAYBOOK=/root/jenkins.yml
-curl -L https://github.com/IQSS/dataverse-jenkins/files/3258484/jenkins.yml.txt > /root/jenkins.yml
 yum install -y ansible git vim-enhanced mlocate wget
 ansible-galaxy install -p roles geerlingguy.jenkins
-ansible-playbook $PLAYBOOK
+ansible-playbook jenkins.yml
