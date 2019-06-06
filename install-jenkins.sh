a5783122cecb1ac0bea7515c8ed3f8c85d2c918a
Philip Durbin <philipdurbin@gmail.com>
2019-06-06 10:32:29 -0400

    install docker for docker-aio #1

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-yum install -y ansible git maven vim-enhanced mlocate wget
+yum install -y ansible git maven zip vim-enhanced mlocate wget
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io
+systemctl start docker
+systemctl enable docker
+usermod -aG docker centos
 ansible-galaxy install -p roles geerlingguy.jenkins
 ansible-playbook jenkins.yml
