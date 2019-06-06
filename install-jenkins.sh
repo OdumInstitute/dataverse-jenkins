963fe0779f24dd860c45133db9e4e57ae4ba75f0
Philip Durbin <philip_durbin@harvard.edu>
2019-06-05 20:44:14 -0400

    install github plugin #1

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-yum install -y ansible git vim-enhanced mlocate wget
+yum install -y ansible git maven vim-enhanced mlocate wget
 ansible-galaxy install -p roles geerlingguy.jenkins
 ansible-playbook jenkins.yml
