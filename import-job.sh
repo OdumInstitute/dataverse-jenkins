7bb595a89aa7e59085174be5f48c9b98cf50a64b
Don Sizemore <don.sizemore@gmail.com>
2019-07-31 15:03:03 -0400

    rename config.xml to IQSS-Dataverse-Develop.xml, add IQSS-Dataverse-PullRequest.xml

--- import-job.sh
+++ import-job.sh
@@ -2,4 +2,4 @@
 SERVER=http://localhost:8080
 USERNAME=admin
 PASSWORD=admin
-java -jar /opt/jenkins-cli.jar -s $SERVER -auth $USERNAME:$PASSWORD create-job IQSS-dataverse-develop < config.xml
+java -jar /opt/jenkins-cli.jar -s $SERVER -auth $USERNAME:$PASSWORD create-job IQSS-dataverse-develop < IQSS-Dataverse-Develop.xml
