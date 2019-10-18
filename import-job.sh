ba39981538a09cd98e1f6f485efcc0091bd4ea0a
donsizemore <don.sizemore@gmail.com>
2019-10-18 13:54:11 -0400

    #19 update default usage, allow arbitrary job specification

--- import-job.sh
+++ import-job.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+
+if [ ! -z $1 ]; then
+   JOB=$1
+else
+   JOB="IQSS-dataverse-develop"
+fi
+
 SERVER=http://localhost:8080
 USERNAME=admin
 PASSWORD=admin
-java -jar /opt/jenkins-cli.jar -s $SERVER -auth $USERNAME:$PASSWORD create-job IQSS-dataverse-develop < IQSS-Dataverse-Develop.xml
+java -jar /opt/jenkins-cli.jar -s $SERVER -auth $USERNAME:$PASSWORD create-job $JOB < $JOB.xml
