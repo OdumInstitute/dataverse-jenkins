9e8a6f5440d86487d59177293c6f2ad3500fd035
don.sizemore <don.sizemore@github.com>
2021-02-22 16:26:23 -0500

    #42 tidy echoes, afternoon checkpoint

--- podman/entrypoint.bash
+++ podman/entrypoint.bash
@@ -27,6 +27,7 @@ done
 
 # clone IQSS/dataverse:develop
 git clone -b $IQSS_BRANCH $IQSS_REPO
+echo ""
 
 # if we have PR vars, merge with develop
 if [ ! -z $PR_BRANCH ];
@@ -39,9 +40,11 @@ echo "find stdout and stderr in /dataverse/mvn.out"
 cd /dataverse && source /etc/profile.d/maven.sh && \
    mvn -Djacoco.skip.instrument=false -DcompilerArgument=-Xlint:unchecked test -P all-unit-tests -T 2C package > /dataverse/mvn.out 2>&1
 echo ""
+
 echo "generating surefire reports"
 cd /dataverse && source /etc/profile.d/maven.sh && \
    mvn surefire-report:report > /dataverse/surefire.out 2>&1
+echo ""
 
 echo "jacoco instrumentation"
 mkdir /jacoco-tmp
@@ -54,14 +57,13 @@ echo ""
 
 echo "building dvinstall.zip"
 cd /dataverse/scripts/installer && make clean && make dvinstall.zip
-unzip /dataverse/scripts/installer/dvinstall.zip -d /
+unzip -q /dataverse/scripts/installer/dvinstall.zip -d /
 # allow unprivileged installer to write to /dvinstall
 /bin/chmod o+w /dvinstall
 echo "copying instrumented warfile into /dvinstall"
 cp /jacoco-tmp/dataverse.war /dvinstall/dataverse.war
-
-# start postgres
 echo ""
+
 echo "starting postgres"
 sudo -u postgres /usr/pgsql-10/bin/pg_ctl start -D /var/lib/pgsql/data &
 /bin/sleep 1
@@ -83,13 +85,39 @@ echo ""
 # deploy
 echo "deploying dataverse. stdout and stderr in /dvinstall/install.out."
 cd /dvinstall && sudo -u payara python3 install.py -f --config_file=default.config --noninteractive > install.out 2>&1
-curl -X PUT -d FAKE http://localhost:8080/api/admin/settings/:DoiProvider
+echo "setting FAKE DOI Provider"
+curl -s -X PUT -d FAKE http://localhost:8080/api/admin/settings/:DoiProvider
+echo ""
 
 # restart payara
 echo "restarting payara..."
 sudo -u payara /usr/local/payara5/bin/asadmin stop-domain
 sudo -u payara /usr/local/payara5/bin/asadmin start-domain
+echo ""
 
-#echo "done, exiting."
+echo "preparing Dataverse to run integration tests"
+curl -s -X PUT -d burrito http://localhost:8080/api/admin/settings/BuiltinUsers.KEY
+export API_TOKEN=`cat /dvinstall/setup-all.*.log |grep apiToken | jq .data.apiToken |tr -d \"`
+curl -H "X-Dataverse-key: $API_TOKEN" --header "Content-Type: application/json" --request POST \
+  --data '{"assignee":":authenticated-users","role":"fullContributor"}' \
+  http://localhost:8080/api/dataverses/root/assignments
+curl -H "X-Dataverse-key: $API_TOKEN" -X POST http://localhost:8080/api/dataverses/root/actions/:publish
+echo "running integration tests"
+cd /dataverse && source /etc/profile.d/maven.sh && \
+   mvn test -Dtest=DataversesIT,DatasetsIT,SwordIT,AdminIT,BuiltinUsersIT,UsersIT,UtilIT,ConfirmEmailIT,FileMetadataIT,FilesIT,SearchIT,InReviewWorkflowIT,HarvestingServerIT,MoveIT,MakeDataCountApiIT,FileTypeDetectionIT,EditDDIIT,ExternalToolsIT,AccessIT,DuplicateFilesIT,DownloadFilesIT,LinkIT
+echo ""
+
+echo "restarting payara to write out jacoco info..."
+sudo -u payara /usr/local/payara5/bin/asadmin stop-domain
+sudo -u payara /usr/local/payara5/bin/asadmin start-domain
+
+echo "merging code coverage reports"
+/usr/bin/java -jar /jacoco/lib/jacococli.jar merge /usr/local/payara5/glassfish/domains/domain1/config/jacoco.exec /dataverse/target/jacoco.exec --destfile /dataverse/target/jacoco_merged.exec
+
+echo "writing code coverage reports"
+/usr/bin/java -jar /jacoco/lib/jacococli.jar report --classfiles /dataverse/target/classes --sourcefiles /dataverse/src/main/java --html /dataverse/target/coverage-it /dataverse/target/jacoco_merged.exec
+echo ""
+
+echo "done, sleeping."
 #sudo -u payara /usr/local/payara5/bin/asadmin stop-domain
 sleep infinity
